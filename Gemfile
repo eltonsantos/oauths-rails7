898794fa03bf3d9341c0d6cbea2666671280881d
Elton Santos <eltonaxl@hotmail.com>
2023-05-16 19:21:52 -0300

    Google authentication and dotenv ok

--- Gemfile
+++ Gemfile
@@ -44,6 +44,13 @@ gem "bootsnap", require: false
 
 gem 'devise'
 
+gem "omniauth-google-oauth2"
+
+gem "omniauth-rails_csrf_protection"
+
+gem 'dotenv', require: 'dotenv-rails'
+gem 'dotenv-rails', require: 'dotenv/rails-now'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+
+  # FOR TUTORIAL 1
+  def self.from_google(u)
+    create_with(uid: u[:uid], name: u[:name], provider: 'google', password: Devise.friendly_token[0, 20]).find_or_create_by!(email: u[:email])
+  end
+
+  # FOR TUTORIAL 2
+  # def self.from_omniauth(auth)
+  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #     user.email = auth.info.email
+  #     user.password = Devise.friendly_token[0, 20]
+  #   end
+  # end
 end
